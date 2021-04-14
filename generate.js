b131e8fdaae3b673deff3ff031c262401fd96d61
kxu <kevin@westsidelabs.io>
2021-04-14 15:43:59 -0400

    Refactor and consolidate mapping scripts

--- generate.js
+++ generate.js
@@ -10,12 +10,10 @@
  * Error Handling:
  *  - What happens when unable to find directions?
  */
-const {Client, Status} = require("@googlemaps/google-maps-services-js");
-const fs = require("fs");
+const { Client, Status } = require("@googlemaps/google-maps-services-js");
 
 // this will come from Form input - selected using Places API
-/*
-var points = [
+const points = [
     "Atlanta, GA",
     "Williams Junction, AR",
     "Lake Meredith, TX",
@@ -25,43 +23,21 @@ var points = [
     "Big Sur, CA",
     "1020 6th St, Santa Monica, CA"
 ];
-*/
-var points = [
-    "Atlanta, GA",
-    "Louisville, KY",
-    "Chicago, IL",
-    "Saint Paul, MN",
-    "Grand Tetons, WY",
-    "Seattle, WA",
-    "Bend, OR",
-    "Los Angeles, CA"
-];
 
 const client = new Client({});
-client
-    .directions({
-        params: {
-            origin: points.shift(),
-            destination: points.pop(),
-            waypoints: points,
-            key: process.env.GOOGLE_API_KEY
-        },
-        timeout: 1000,  // milliseconds
-    })
-    .then((r) => {
-        console.log("GET /maps/api/directions success - generating geoJSON...");
-        console.log("dumpign results", r.data);
-
-        var filename = "response.js";
-        var text = "var response = " + JSON.stringify(r.data) + ";";
-        fs.writeFile(filename, text, function(err) {
-            if (err) {
-                return console.log("ERR with file write", err);
-            }
-            console.log(`created ${filename}`);
-        });
-    })
-    .catch(err => {
-        console.log("ERR:", err);
-        console.log("ERR data:", err.data);
-    });
+client.directions({
+    params: {
+        origin: points.shift(),
+        destination: points.pop(),
+        waypoints: points,
+        key: process.env.GOOGLE_API_KEY
+    },
+    timeout: 5000,  // milliseconds
+})
+.then((r) => {
+    //console.log(JSON.stringify(r.data));
+    console.log(JSON.stringify(r.data, null, 2));
+})
+.catch(err => {
+    console.log("ERR:", err);
+});
--- kv.js
+++
@@ -1,1 +0,0 @@
-var mb = "pk.eyJ1Ijoia3h1MTYiLCJhIjoiY2p5NXh1bzZqMGNrMzNkbzB1bjlsazluaCJ9.LWKf9jAXZmDmKgAWA-IS9g";
